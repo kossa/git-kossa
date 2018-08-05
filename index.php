836b91bcbaad9030071231b07b71cab63e21d545
kouceyla <hadjikouceyla@gmail.com>
2018-08-05 13:44:22 +0100

    Renomer index.html

---
+++ index.php
@@ -0,0 +1,4 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quisquam voluptatem, minus, hic beatae expedita totam, modi saepe eveniet magnam maiores in dignissimos aliquid delectus et ad illo ea atque.</p>
+
+<h1>Hello world</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit tempora asperiores, iste laborum optio praesentium sit, tempore at, dolorem, eveniet ad officia? Suscipit rem corporis dicta saepe, velit, odit nisi?</p>
