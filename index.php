45de09216b0b8e8b91472e8c66593878fd4407c8
siphax <syphax_ka@hotmail.com>
2018-08-06 12:51:15 +0200

    sif index.php

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 <h1>Hello world</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit tempora asperiores, iste laborum optio praesentium sit, tempore at, dolorem, eveniet ad officia? Suscipit rem corporis dicta saepe, velit, odit nisi?</p>
 
-<p>paragraphe dev</p>
+<p>kossa/siff</p>
